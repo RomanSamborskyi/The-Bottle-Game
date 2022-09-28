a1d0c5d6835281ca12eceb04dfe6ea621cea18ab
RomanSamborskyi <r.samborskyi@gmail.com>
2022-09-28 10:07:38 +0200

    dark mode switch button

--- Bottle Game/ContentView.swift
+++ Bottle Game/ContentView.swift
@@ -19,12 +19,14 @@ struct ContentView: View {
     @State private var isRotated = 0.0
     @State var showSettings: Bool = false
     @State var animatedButton: Bool = false
-    @Environment (\.colorScheme) var colorscheme
-  
+    @Environment (\.colorScheme) var colorScheme
+    @AppStorage("isDark") private var isDark = false
+    
     var body: some View {
-        ZStack{
+        NavigationView{
+           ZStack{
             //Background gradient
-            LinearGradient(gradient: colorscheme == .dark ? Gradient(colors: [
+            LinearGradient(gradient: isDark ? Gradient(colors: [
                 Color.black,
                 Color.blue,
                 Color.black,
@@ -57,12 +59,13 @@ struct ContentView: View {
                         .font(.title)
                         .foregroundColor(Color.init(red: 0.4, green: 11, blue: 4, opacity: 0.2))
                         .background(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2))
-                        .shadow(color: colorscheme == .light ? .blue : .black, radius:animatedButton ? 10 : 45, x: 0, y: animatedButton ? 30 : 0)
+                        .shadow(color: isDark ? .black : .blue, radius:animatedButton ? 10 : 45, x: 0, y: animatedButton ? 30 : 0)
                         .scaleEffect(animatedButton ? 1.15 : 1.0)
                 }
             }.onAppear(perform: animatedBuuton)
             VStack{
-                Spacer()
+             Spacer()
+                
             Button( action: {
                 showSettings.toggle()
             }){
@@ -70,19 +73,26 @@ struct ContentView: View {
                     .foregroundColor(Color.gray)
                 }
             }
-        }
-        .sheet(isPresented: $showSettings, content:{ AbotAppView()})
-    }
+           }.sheet(isPresented: $showSettings, content:{ AbotAppView()})
+                .navigationTitle("")
+                .navigationBarItems(leading: Button(action:{
+                    isDark.toggle()
+                }){
+                    Text(Image(systemName: isDark ? "moon.fill" : "sun.max.fill"))
+                        .foregroundColor(isDark ? Color.purple : Color.yellow)
+                }.buttonStyle(.bordered).tint(isDark ? .gray : .blue))
+        }.environment(\.colorScheme, isDark ? .dark : .light)
+}
     func animatedBuuton(){
         guard !animatedButton else {return}
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
             withAnimation(Animation.easeInOut(duration: 2.0).repeatForever()){
                 animatedButton.toggle()
+               
             }
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Bottle Game/Settings/AbotAppView.swift
+++ Bottle Game/Settings/AbotAppView.swift
@@ -9,70 +9,73 @@ import SwiftUI
 
 struct AbotAppView: View {
     @State var animatedButton : Bool = false
-    @Environment (\.colorScheme) var colorscheme
+    @Environment (\.colorScheme) var colorScheme
+    @AppStorage("isDark") private var isDark = false
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: colorscheme == .dark ? Gradient(colors: [
-                Color.black,
-                Color.blue,
-                Color.black,
-            ]) : Gradient(colors: [
-                Color.white,
-                Color.blue,
-                Color.white,
-            ]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing
-            ).ignoresSafeArea(.all,edges: .all)
-           VStack{
-        Image("me")
-            .resizable()
-            .frame(width: 270, height: 245)
-            .clipShape(Circle())
-            .shadow(color: Color.black, radius: 10)
-            .padding()
-            
-            Text("Hello 👋, my name is Roman Samborskyi and I'm passionate about programming and just learning it 📖This application is my first app.This is a simple game, bottle.So enjoy it 😉")
-                .font(.callout)
-                .foregroundColor(Color.black)
-                .bold()
-                .padding()
-            
-               Text("If you enjoy this app, you can by me a cup of coffee ☕️")
-                   .font(.callout)
-                   .foregroundColor(Color.black)
-                   .bold()
-                   .padding()
-                  
+        NavigationView{
+            ZStack{
+                LinearGradient(gradient: isDark ? Gradient(colors: [
+                    Color.black,
+                    Color.blue,
+                    Color.black,
+                ]) : Gradient(colors: [
+                    Color.white,
+                    Color.blue,
+                    Color.white,
+                ]),
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing
+                ).ignoresSafeArea(.all,edges: .all)
+                VStack{
+                    Image("me")
+                        .resizable()
+                        .frame(width: 270, height: 245)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black, radius: 10)
+                        .padding()
+                    
+                    Text("Hello 👋, my name is Roman Samborskyi and I'm passionate about programming and just learning it 📖This application is my first app.This is a simple game, bottle.So enjoy it 😉")
+                        .font(.callout)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        .padding()
+                    
+                    Text("If you enjoy this app, you can by me a cup of coffee ☕️")
+                        .font(.callout)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        .padding()
+                    
                     Button(action:{
-                       //
+                        //
                     }){
                         ZStack{
                             RoundedRectangle(cornerRadius: 12)
                                 .stroke(lineWidth: 3)
                                 .foregroundColor(Color.init(red: 0.4, green: 11, blue: 4, opacity: 0.2))
                                 .frame(width: 110, height: 55, alignment: .center)
-                                
-                        Text("$ 1.00")
-                            .font(.body)
-                            .foregroundColor(Color.init(red: 0.4, green: 11, blue: 4, opacity: 0.2))
-                            .bold()
-                        }.shadow(color: colorscheme == . light ? .blue : .black, radius: animatedButton ? 10 : 35, x: 0, y: animatedButton ? 30 : 0)
+                            
+                            Text("$ 1.00")
+                                .font(.body)
+                                .foregroundColor(Color.init(red: 0.4, green: 11, blue: 4, opacity: 0.2))
+                                .bold()
+                        }.shadow(color: colorScheme == . light ? .blue : .black, radius: animatedButton ? 10 : 35, x: 0, y: animatedButton ? 30 : 0)
                             .scaleEffect(animatedButton ? 1.15 : 1.0)
                     }.padding(40)
-                     
                     
-               Text("Also you can connect with me by email 📧")
-                   .font(.body)
-                   .foregroundColor(Color.black)
-                   .bold()
-                   .padding(10)
-               Text("r.samborskyi@gmail.com")
-                   .textContentType(.emailAddress)
-                   .font(.headline)
-               Spacer()
-           }.onAppear(perform: animatedButtonFunc)
-        }
+                    
+                    Text("Also you can connect with me by email 📧")
+                        .font(.body)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        .padding(10)
+                    Text("r.samborskyi@gmail.com")
+                        .textContentType(.emailAddress)
+                        .font(.headline)
+                    Spacer()
+                }.onAppear(perform: animatedButtonFunc)
+            }
+        }.environment(\.colorScheme, isDark ? .dark : .light)
     }
     func animatedButtonFunc(){
         guard !animatedButton else {return}
